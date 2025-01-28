e9b30f31a4c05c6dd0137e7fcd1d2fabeef6994d
AlpinDale <alpindale@gmail.com>
2025-01-28 18:00:30 +0000

    typo

--- kernels/xqa/torch_bindings.cpp
+++ kernels/xqa/torch_bindings.cpp
@@ -3,7 +3,7 @@
 
 TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, xqa_ops) {
   // PagedAttention xqa.
-  ops.def(
+  xqa_ops.def(
       "xqa_paged_attention("
       "    Tensor! out,"
       "    Tensor query, Tensor key_value_cache,"
@@ -11,7 +11,7 @@ TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, xqa_ops) {
       "    float scale, Tensor block_tables, Tensor seq_lens, int block_size,"
       "    int max_seq_len, str kv_cache_dtype,"
       "    float k_scale, float v_scale) -> ()");
-  ops.impl("xqa_paged_attention", torch::kCUDA, &xqa_paged_attention);
+  xqa_ops.impl("xqa_paged_attention", torch::kCUDA, &xqa_paged_attention);
 }
 
 REGISTER_EXTENSION(TORCH_EXTENSION_NAME)
