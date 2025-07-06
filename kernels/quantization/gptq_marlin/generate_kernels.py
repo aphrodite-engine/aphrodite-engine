45843baeab9e653c7bbeb94f32f6c71f997ebba3
AlpinDale <alpindale@gmail.com>
2025-07-07 02:56:25 +0430

    feat: add native and emulated FP4 support (NVFP4)

--- kernels/quantization/gptq_marlin/generate_kernels.py
+++ kernels/quantization/gptq_marlin/generate_kernels.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0
 import glob
 import itertools
 import os
@@ -24,26 +25,22 @@ TEMPLATE = ("template __global__ void Marlin<"
             "{{thread_k_blocks}}, "
             "{{'true' if m_block_size_8 else 'false'}}, "
             "{{stages}}, "
-            "{{'true' if has_act_order else 'false'}}, "
-            "{{'true' if has_zp else 'false'}}, "
             "{{group_blocks}}, "
             "{{'true' if is_zp_float else 'false'}}>"
             "( MARLIN_KERNEL_PARAMS );")
 
 # int8 with zero point case (aphrodite::kU8) is also supported,
 # we don't add it to reduce wheel size.
-SCALAR_TYPES = [
-    "aphrodite::kU4", "aphrodite::kU4B8", "aphrodite::kU8B128", "aphrodite::kFE4M3fn",
-    "aphrodite::kFE2M1f"
-]
-THREAD_CONFIGS = [(128, 128, 256), (64, 256, 256), (64, 128, 128)]
+SCALAR_TYPES = ["aphrodite::kU4", "aphrodite::kU4B8", "aphrodite::kU8B128", "aphrodite::kFE4M3fn"]
+THREAD_CONFIGS = [(128, 128, 256), (64, 256, 256), (64, 128, 128),
+                  (128, 64, 128)]
 
 THREAD_M_BLOCKS = [0.5, 1, 2, 3, 4]
 # group_blocks:
 #   = 0 : act order case
 #   = -1 : channelwise quantization
 #   > 0 : group_size=16*group_blocks
-GROUP_BLOCKS = [0, -1, 1, 2, 4, 8]
+GROUP_BLOCKS = [0, -1, 2, 4, 8]
 DTYPES = ["fp16", "bf16"]
 
 
@@ -61,7 +58,7 @@ def generate_new_kernels():
 
             # act order case only support gptq-int4 and gptq-int8
             if group_blocks == 0 and scalar_type not in [
-                    "aphrodite::kU4", "aphrodite::kU8B128"
+                    "aphrodite::kU4B8", "aphrodite::kU8B128"
             ]:
                 continue
             if thread_configs[2] == 256:
@@ -76,12 +73,6 @@ def generate_new_kernels():
             # for fp8
             if scalar_type == "aphrodite::kFE4M3fn" and group_blocks not in [-1, 8]:
                 continue
-            # nvfp4 only supports group_size == 16
-            if scalar_type == "aphrodite::kFE2M1f" and group_blocks not in [1, 2]:
-                continue
-            # other quantization methods don't support group_size = 16
-            if scalar_type != "aphrodite::kFE2M1f" and group_blocks == 1:
-                continue
 
             k_blocks = thread_configs[0] // 16
             n_blocks = thread_configs[1] // 16
@@ -89,20 +80,28 @@ def generate_new_kernels():
 
             c_dtype = "half" if dtype == "fp16" else "nv_bfloat16"
 
-            template_str = jinja2.Template(TEMPLATE).render(
-                scalar_t=c_dtype,
-                w_type_id=scalar_type + ".id()",
-                threads=threads,
-                thread_m_blocks=max(m_blocks, 1),
-                thread_n_blocks=n_blocks,
-                thread_k_blocks=k_blocks,
-                m_block_size_8=m_blocks == 0.5,
-                stages="pipe_stages",
-                group_blocks=group_blocks,
-                is_zp_float=False,
-            )
+            is_zp_float_list = [False]
+            if dtype == "fp16" and scalar_type == "aphrodite::kU4" and \
+                    group_blocks == 4:
+                # HQQ (is_zp_float = true) only supports
+                # 4bit quantization and fp16
+                is_zp_float_list.append(True)
 
-            all_template_str_list.append(template_str)
+            for is_zp_float in is_zp_float_list:
+                template_str = jinja2.Template(TEMPLATE).render(
+                    scalar_t=c_dtype,
+                    w_type_id=scalar_type + ".id()",
+                    threads=threads,
+                    thread_m_blocks=max(m_blocks, 1),
+                    thread_n_blocks=n_blocks,
+                    thread_k_blocks=k_blocks,
+                    m_block_size_8=m_blocks == 0.5,
+                    stages="pipe_stages",
+                    group_blocks=group_blocks,
+                    is_zp_float=is_zp_float,
+                )
+
+                all_template_str_list.append(template_str)
 
         file_content = FILE_HEAD + "\n\n"
         file_content += "\n\n".join(all_template_str_list) + "\n\n}\n"
