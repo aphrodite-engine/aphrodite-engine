f208ad563391a6c01a3da89231080eae0f06273c
AlpinDale <alpindale@gmail.com>
2025-08-24 20:56:13 +0000

    compile fa

--- setup.py
+++ setup.py
@@ -639,14 +639,14 @@ if not envs.APHRODITE_USE_PRECOMPILED:
         ext_modules.append(CMakeExtension(name="aphrodite._rocm_C"))
 
     if _is_cuda():
-        # ext_modules.append(CMakeExtension(
-        #     name="aphrodite.aphrodite_flash_attn._vllm_fa2_C"))
+        ext_modules.append(CMakeExtension(
+            name="aphrodite.aphrodite_flash_attn._vllm_fa2_C"))
         # Build FA3/_flashmla when using precompiled artifacts or nvcc >= 12.3.
         if envs.APHRODITE_USE_PRECOMPILED or \
                 get_nvcc_cuda_version() >= Version("12.3"):
-            # ext_modules.append(
-            #     CMakeExtension(
-            #         name="aphrodite.aphrodite_flash_attn._vllm_fa3_C"))
+            ext_modules.append(
+                CMakeExtension(
+                    name="aphrodite.aphrodite_flash_attn._vllm_fa3_C"))
             # Optional since this doesn't get built (produce an .so file) when
             # not targeting a hopper system
             ext_modules.append(
