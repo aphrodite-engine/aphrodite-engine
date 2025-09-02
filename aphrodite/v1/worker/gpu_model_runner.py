1babf544b9cda81fca607660a98be91b71d5f20b
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2025-09-03 01:57:49 +0430

    chore: faster LoRA startup (#1454)

--- aphrodite/v1/worker/gpu_model_runner.py
+++ aphrodite/v1/worker/gpu_model_runner.py
@@ -2247,6 +2247,7 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
         uniform_decode: bool = False,
         skip_eplb: bool = False,
         is_profile: bool = False,
+        remove_lora: bool = True,
     ) -> tuple[torch.Tensor, torch.Tensor]:
         """
         Run a dummy forward pass to warm up/profile run or capture the
@@ -2263,6 +2264,7 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
             uniform_decode: If True, the batch is a uniform decode batch.
             skip_eplb: If True, skip EPLB state update.
             is_profile: If True, this is a profile run.
+            remove_lora: If False, dummy LoRAs are not destroyed after the run
         """
         assert cudagraph_runtime_mode in {
             CUDAGraphMode.NONE, CUDAGraphMode.PIECEWISE, CUDAGraphMode.FULL
@@ -2348,7 +2350,7 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
                         attn_metadata[layer_name] = attn_metadata_i
 
         with self.maybe_dummy_run_with_lora(self.lora_config,
-                                            num_scheduled_tokens):
+                                            num_scheduled_tokens, remove_lora):
             if self.supports_mm_inputs:
                 input_ids = None
                 inputs_embeds = self.inputs_embeds[:num_tokens]
@@ -2776,11 +2778,13 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
                                         cudagraph_runtime_mode=CUDAGraphMode.NONE,
                                         force_attention=force_attention,
                                         uniform_decode=uniform_decode,
-                                        skip_eplb=True)
+                                        skip_eplb=True,
+                                        remove_lora=False)
                     self._dummy_run(num_tokens,
                                     cudagraph_runtime_mode=cudagraph_runtime_mode,
                                     uniform_decode=uniform_decode,
-                                    skip_eplb=True)
+                                    skip_eplb=True,
+                                    remove_lora=False)
         else:
             for num_tokens in compilation_cases:
                 # We skip EPLB here since we don't want to record dummy metrics
@@ -2796,11 +2800,14 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
                                     cudagraph_runtime_mode=CUDAGraphMode.NONE,
                                     force_attention=force_attention,
                                     uniform_decode=uniform_decode,
-                                    skip_eplb=True)
+                                    skip_eplb=True,
+                                    remove_lora=False)
                 self._dummy_run(num_tokens,
                                 cudagraph_runtime_mode=cudagraph_runtime_mode,
                                 uniform_decode=uniform_decode,
-                                skip_eplb=True)
+                                skip_eplb=True,
+                                remove_lora=False)
+        self.maybe_remove_all_loras(self.lora_config)
 
     def initialize_attn_backend(self, kv_cache_config: KVCacheConfig) -> None:
         """
