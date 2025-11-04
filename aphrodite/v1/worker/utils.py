86effb4659b51e740380c483cb2c788b84187726
AlpinDale <alpindale@gmail.com>
2025-11-04 05:40:18 +0000

    [hybrid] simpler algorithm to find `kernel_block_size`
    Signed-off-by: AlpinDale <alpindale@gmail.com>

--- aphrodite/v1/worker/utils.py
+++ aphrodite/v1/worker/utils.py
@@ -138,6 +138,7 @@ class AttentionGroup:
     metadata_builders: list[AttentionMetadataBuilder]
     layer_names: list[str]
     kv_cache_spec: KVCacheSpec
+    kv_cache_group_id: int
 
     @staticmethod
     def create_with_metadata_builders(
@@ -146,13 +147,14 @@ class AttentionGroup:
         kv_cache_spec: KVCacheSpec,
         aphrodite_config: AphroditeConfig,
         device: torch.device,
+        kv_cache_group_id: int,
         num_metadata_builders: int = 1,
     ) -> "AttentionGroup":
         metadata_builders = [
             backend.get_builder_cls()(kv_cache_spec, layer_names, aphrodite_config, device)
             for _ in range(num_metadata_builders)
         ]
-        return AttentionGroup(backend, metadata_builders, layer_names, kv_cache_spec)
+        return AttentionGroup(backend, metadata_builders, layer_names, kv_cache_spec, kv_cache_group_id)
 
     def get_metadata_builder(self, ubatch_id: int = 0) -> AttentionMetadataBuilder:
         assert len(self.metadata_builders) > ubatch_id
