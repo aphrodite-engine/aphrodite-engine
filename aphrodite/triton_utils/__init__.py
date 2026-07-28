d19cd0ab542e7c3f7fdbeca40599753347e6fe58
AlpinDale <alpindale@gmail.com>
2026-07-28 04:19:24 +0000

    [sync] [ROCm] [Model] Enable TML inkling (#48841)
    Upstream-vLLM: 0906123953a5a253cc0cc4f26e548ca08ece102a
    Co-authored-by: TJian <tunjian.tan@embeddedllm.com>

--- aphrodite/triton_utils/__init__.py
+++ aphrodite/triton_utils/__init__.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+from importlib import import_module
 from typing import TYPE_CHECKING
 
 from aphrodite.triton_utils.importing import (
@@ -12,10 +13,17 @@ if TYPE_CHECKING or HAS_TRITON:
     import triton
     import triton.language as tl
     import triton.language.extra.libdevice as tldevice
+
+    gluon = import_module("triton.experimental.gluon")
+    gl = import_module("triton.experimental.gluon.language")
+    aggregate = import_module("triton.language.core")._aggregate
 else:
     triton = TritonPlaceholder()
     tl = TritonLanguagePlaceholder()
     tldevice = TritonLanguagePlaceholder()
+    gluon = TritonLanguagePlaceholder()
+    gl = TritonLanguagePlaceholder()
+    aggregate = TritonLanguagePlaceholder()
 
 from aphrodite.triton_utils.tensor_descriptor import use_tensor_descriptor
 
@@ -29,5 +37,8 @@ __all__ = [
     "tldevice",
     "LOG2E",
     "LOGE2",
+    "gluon",
+    "gl",
+    "aggregate",
     "use_tensor_descriptor",
 ]
